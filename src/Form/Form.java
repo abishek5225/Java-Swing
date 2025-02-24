08c8bd80fa5fb0482ae096179a0b0a21040a2350
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-24 07:44:48 +0545

    updated

---
+++ src/Form/Form.java
@@ -0,0 +1,11 @@
+package Form;
+
+import javax.swing.*;
+
+public class Form {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("User Form");
+        frame.setSize(350, 250);
+    }
+}
