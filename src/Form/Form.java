0ee2b8f899dfc2041fc1af772c8820d48a6e0c4f
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-24 07:52:22 +0545

    updated

--- src/Form/Form.java
+++ src/Form/Form.java
@@ -11,6 +11,14 @@ public class Form {
 
         JFrame frame = new JFrame("User Form");
         frame.setSize(350, 250);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(30, 30,30));
+
+        JLabel label= new JLabel("Enter Name:");
+        label.setBounds(20,20,100,25);
+        label.setForeground(Color.WHITE);
+
+
 
     }
 }
